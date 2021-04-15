077aafd7f19dd2b5ad9e61ec2abad496ad50982a
Cedric Scherer <cedricphilippscherer@gmail.com>
2021-04-15 20:22:07 +0200

    add documentation datasets

---
+++ R/data.R
@@ -0,0 +1,37 @@
+#' Berlin districts (WGS 84)
+#'
+#' A shape file containing the shape of Berlin.
+#'
+#' @source \url{https://daten.odis-berlin.de/en/dataset/bezirksgrenzen}
+"sf_berlin"
+
+#' Berlin green spaces (WGS 84)
+#'
+#' A shape file containing the shape of all green spaces (defined as natural
+#' areas and landuse categories "forest", "grass", "meadow", "nature_reserve",
+#' "scrub", "heath", "beach", and "cliff") in Berlin.
+#'
+#' @source \url{https://download.geofabrik.de/europe/germany/berlin.html}
+"sf_green"
+
+#' Berlin waterways (WGS 84)
+#'
+#' A shape file containing the shape of all waterways found in Berlin.
+#'
+#' @source \url{https://download.geofabrik.de/europe/germany/berlin.html}
+"sf_water"
+
+#' Berlin imperviousness levels (WGS 84)
+#'
+#' A raster file containing the imperviousness levels in Berlin on a 10m resolution.
+#'
+#' @source \url{https://download.geofabrik.de/europe/germany/berlin.html}
+"ras_imp_orig"
+
+#' Berlin metro station (WGS 84)
+#'
+#' A shape file containing the locations of metro stations in Berlin. Used as
+#'example dataset to illustrate how to add points to the template basemap.
+#'
+#' @source \url{https://download.geofabrik.de/europe/germany/berlin.html}
+"sf_metro"
