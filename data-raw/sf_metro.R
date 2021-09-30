202d7e27eca8e8da3c54abffd7aa7fd6292cd336
z3tt <cedricphilippscherer@gmail.com>
2021-09-30 16:18:45 +0200

    fix non-ACII symbols sf_metro

--- data-raw/sf_metro.R
+++ data-raw/sf_metro.R
@@ -13,12 +13,20 @@ sf_districts <- sf::read_sf(json_file)
 
 ## Berlin metro stations (WGS 84)
 sf_metro <-
-  suppressMessages(sf::read_sf(dsn = glue::glue("{shp_path}/gis_osm_transport_free_1.shp"),
-                               layer = "gis_osm_transport_free_1") %>%
-                     dplyr::filter(
-                       fclass %in% c("railway_station"),
-                       stringr::str_detect(name, "^U |^S ")
-                      ) %>%
-                     sf::st_intersection(sf_districts))
+  suppressMessages(
+    sf::read_sf(dsn = glue::glue("{shp_path}/gis_osm_transport_free_1.shp"),
+                layer = "gis_osm_transport_free_1") %>%
+      dplyr::filter(
+        fclass %in% c("railway_station"),
+        stringr::str_detect(name, "^U |^S ")
+      ) %>%
+      dplyr::mutate(
+        name = str_replace_all(name, "ß", "ss"),
+        name = str_replace_all(name, "ä", "ae"),
+        name = str_replace_all(name, "ö", "oe"),
+        name = str_replace_all(name, "ü", "ue"),
+      ) %>%
+      sf::st_intersection(sf_districts)
+  )
 
 usethis::use_data(sf_metro, overwrite = TRUE)
