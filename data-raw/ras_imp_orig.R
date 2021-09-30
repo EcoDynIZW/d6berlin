655cbc351f6dd5db70d3bff1b6fa3e9ea8a1200a
z3tt <cedricphilippscherer@gmail.com>
2021-09-30 15:48:41 +0200

    test terra::rast as import

--- data-raw/ras_imp_orig.R
+++ data-raw/ras_imp_orig.R
@@ -7,6 +7,6 @@ path <-
 ## PREPARE DATA --------------------------------------------------------------
 ## Berlin imperviousness levels (WGS 84)
 ## Source: Copernicus
-ras_imp_orig <- raster::raster(path)
+ras_imp_orig <- terra::rast(path)
 
 usethis::use_data(ras_imp_orig, overwrite = TRUE)
