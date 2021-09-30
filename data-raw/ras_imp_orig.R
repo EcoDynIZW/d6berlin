4ff6ee0164464f0533529f4b0ab9b3213d19d81e
z3tt <cedricphilippscherer@gmail.com>
2021-09-30 17:05:20 +0200

    trial `values = TRUE`

--- data-raw/ras_imp_orig.R
+++ data-raw/ras_imp_orig.R
@@ -7,7 +7,8 @@ path <-
 ## PREPARE DATA --------------------------------------------------------------
 ## Berlin imperviousness levels (WGS 84)
 ## Source: Copernicus
-#ras_imp_orig <- raster::raster(path)
-ras_imp_orig <- lazyraster::lazyraster(path)
+ras_imp_orig <- raster::raster(path, values = TRUE)
+#ras_imp_orig <- terra::rast(path)
+#ras_imp_orig <- lazyraster::lazyraster(path)
 
 usethis::use_data(ras_imp_orig, overwrite = TRUE)
