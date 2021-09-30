235933f3f1e4f2e0c40e235073703704421e327e
CGMossa <cgmossa@gmail.com>
2021-09-30 16:19:35 +0200

    This solves the "path" issue

--- data-raw/ras_imp_orig.R
+++ data-raw/ras_imp_orig.R
@@ -9,6 +9,8 @@ path <-
 ## PREPARE DATA --------------------------------------------------------------
 ## Berlin imperviousness levels (WGS 84)
 ## Source: Copernicus
-ras_imp_orig <- raster::raster(path)
+ras_imp_orig <-
+  raster::raster("inst/imperviousness_berlin_copernicus_raster_10m_2018_3035.tif")
+ras_imp_orig <- ras_imp_orig * 1
 
 usethis::use_data(ras_imp_orig, overwrite = TRUE)
