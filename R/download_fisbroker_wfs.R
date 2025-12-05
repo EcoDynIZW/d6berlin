b1d19560dd57cb475f9f3228e9cfe8d50902134f
Moritz Wenzler-Meya <38684163+wenzmo@users.noreply.github.com>
2025-12-05 10:14:47 +0100

    updated download fisbroker

--- R/download_fisbroker_wfs.R
+++ R/download_fisbroker_wfs.R
@@ -13,6 +13,7 @@
 #' url <- "https://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_wfs_alkis_bezirk"
 #' d6berlin::download_fisbroker_wfs(link = url)
 #' }
+
 #### Function
 download_fisbroker_wfs <- function(link) { # a data frame is required, with at least a column for the year of data creation and WFS-link
 
@@ -25,17 +26,13 @@ download_fisbroker_wfs <- function(link) { # a data frame is required, with at l
     layer <- wfs_client$ # layer name (incl. prefix, e. g.: "fis:")
       getCapabilities()$
       getFeatureTypes() |>
-      purrr::map_chr(function(x) {
-        x$getName()
-      })
+      purrr::map_chr(function(x){ x$getName() })
 
-    if (length(layer) > 1) {
-      layer_select <- c(layer)[utils::menu(c(layer), title = "choose layer")]
-    } else {
-      layer_select <- layer
-    }
+    if(length(layer)  > 1){
+    layer_select <- c(layer)[utils::menu(c(layer), title = "choose layer")]
+    } else{layer_select <- layer}
 
-    # if(length(layer) > 1) stop(paste0("This function is not suited for WFS-sets with multiple layers. First layer here: ", layer[1]))
+    #if(length(layer) > 1) stop(paste0("This function is not suited for WFS-sets with multiple layers. First layer here: ", layer[1]))
 
     typename <- unlist(strsplit(layer_select, ":"))[2] # layer name without prefix
 
@@ -49,21 +46,20 @@ download_fisbroker_wfs <- function(link) { # a data frame is required, with at l
       findFeatureTypeByName(layer_select)$
       getDefaultCRS()[1]$input
 
-    link2 <- httr::parse_url(single_link)
-    link2$query <- list(
-      service = "wfs",
-      version = "2.0.0",
-      request = "GetFeature",
-      typenames = typename,
-      srsName = paste0("EPSG:", unlist(strsplit(crs, ":"))[2])
-    ) # applies CRS to shapefile for download
-    request <- httr::build_url(link2)
-    request2 <- sf::st_read(request)
+    link2       <- httr::parse_url(single_link)
+    link2$query <- list(service   = "wfs",
+                        version   = "2.0.0",
+                        request   = "GetFeature",
+                        typenames = typename,
+                        srsName   = paste0("EPSG:", unlist(strsplit(crs, ":"))[2])) # applies CRS to shapefile for download
+    request     <- httr::build_url(link2)
+    request2    <- sf::st_read(request)
 
     return(request2)
+
   }
 
   safely_function <- purrr::safely(base_fun) # skip and save errors, when using this function
-  execution <- safely_function(link) # applies function on every row of input table
+  execution       <- safely_function(link) # applies function on every row of input table
   return(execution$result)
 }
